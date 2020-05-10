2e2d2ab6c413796fae6b0c165c4a83369a54a29e
Valentina Ponamareva <valiazin@gmail.com>
2020-05-10 17:09:33 +0300

    flickity adjustments

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -24,10 +24,22 @@ new Vue({
     methods: {
         next() {
             this.$refs.flickity.next();
+            this.checkArrows();
         },
 
         previous() {
             this.$refs.flickity.previous();
+            this.checkArrows();
+        },
+        checkArrows() {
+            if (this.$refs.flickity.selectedIndex() == 0) {
+                this.$refs.circleBtnPrev.disabled = true;
+            } else if (this.$refs.flickity.selectedIndex() == this.$refs.flickity.slides().length - 1) {
+                this.$refs.circleBtnNext.disabled = true;
+            } else {
+                this.$refs.circleBtnPrev.disabled = false;
+                this.$refs.circleBtnNext.disabled = false;
+            }
         }
     }
 });
