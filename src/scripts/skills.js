839d3782abfcc245861a7961d493a59c87c72b35
Valentina Ponamareva <valiazin@gmail.com>
2020-05-11 14:15:37 +0300

    skills functionality corrections

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -3,15 +3,19 @@ import Vue from "vue";
 const skill = {
     template: "#skill",
     props: ["skill"],
-    methods: {
-        drawColoredCircle() {
+    computed: {
+        getPercentValue() {
             const circle = this.$refs["circle"];
             const dashArray = parseInt(
                 getComputedStyle(circle).getPropertyValue("stroke-dasharray")
             );
             const percent = (dashArray / 100) * (100 - this.skill.percent);
-
-            circle.style.strokeDashoffset = percent;
+            return percent;
+        }
+    },
+    methods: {
+        drawColoredCircle() {            
+            this.$refs["circle"].style.strokeDashoffset = this.getPercentValue;
         },
     },
     mounted() {
