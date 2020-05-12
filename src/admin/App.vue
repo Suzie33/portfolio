5267d9caa6e54cd18aa5f4a7ddf111b2815472ec
Valentina Ponamareva <valiazin@gmail.com>
2020-05-12 13:07:56 +0300

    admin markup

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,21 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  header.header
+    .header__left
+      .header__avatar.avatar
+        img(src='../images/content/user.jpg')
+      .header__name Владимир Астаханов
+      .header__note Панель администрирования
+    .header__right
+      .header__exit
+        a.header__exit(href="#") Выйти
 </template>
+
+<style lang="postcss">
+  @import "normalize.css";
+  @import "../styles/mixins.pcss";
+  @import "../styles/layout/base.pcss";
+
+  body {
+    color: green;
+  }
+</style>
