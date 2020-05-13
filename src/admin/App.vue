52bed15ecc6fef2d792acd15eb243e668fcede26
Valentina Ponamareva <valiazin@gmail.com>
2020-05-13 21:20:42 +0300

    admin markup and stylization

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,21 +1,43 @@
 <template lang="pug">
-  header.header
-    .header__left
-      .header__avatar.avatar
-        img(src='../images/content/user.jpg')
-      .header__name Владимир Астаханов
-      .header__note Панель администрирования
-    .header__right
-      .header__exit
-        a.header__exit(href="#") Выйти
+  .admin
+    header.header
+      .container.header__container
+        .header__left
+          .header__avatar.avatar
+            img(src='../images/content/user.jpg' alt="Avatar").avatar__pic
+          .header__name Владимир Астаханов
+          .header__note Панель администрирования
+        .header__right
+          .header__links
+            a.link(href="#") Выйти
+
+    .admin__navigation
+      .container
+        nav.nav
+          - var navlist = ['Обо мне', 'Работы', 'Отзывы'];
+          ul.nav__list
+            each item in navlist
+              li.nav__item
+                a.nav__link(href="#") #{item}
+
+    main.main
+      .container
+        section.about
+          .about__top
+            h2.section__title Блок &laquo;Обо мне&raquo;
+            button.button-inscription
+              .button-inscription__icon
+                .plus-icon
+              span.button-inscription__text Добавить группу
+
+    
 </template>
 
 <style lang="postcss">
   @import "normalize.css";
   @import "../styles/mixins.pcss";
   @import "../styles/layout/base.pcss";
+  @import "./style.pcss";
 
-  body {
-    color: green;
-  }
+  
 </style>
