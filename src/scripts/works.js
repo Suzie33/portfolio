d5f46fd4dd77cbad7695fdcf2f4d90555bf92f2a
Valentina Ponamareva <valiazin@gmail.com>
2020-05-13 12:02:07 +0300

    works slider functionality

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 
 const previews = {
     template: "#slider-previews",
+    props: ["works", "currentWork"]
 };
 
 const buttons = {
@@ -11,17 +12,23 @@ const buttons = {
 const display = {
     template: "#slider-display",
     components: { buttons, previews },
-    props: ["currentWork"]
+    props: ["currentWork", "works"],
 };
 
 const tags = {
     template: "#slider-tags",
+    props: ["tags"]
 };
 
 const info = {
     template: "#slider-info",
     components: { tags },
-    props: ["currentWork"]
+    props: ["currentWork"],
+    computed: {
+        tagsArray() {
+            return this.currentWork.skills.split(",");
+        }
+    }
 };
 
 
@@ -32,10 +39,39 @@ new Vue ({
     data() {
         return {
             works: [],
-            currentWork: {}
+            currentIndex: 2
         };
     },
+    computed: {
+        currentWork() {
+            return this.works[this.currentIndex];
+        }
+    },
+    watch: {
+        currentIndex(value) {
+            this.makeInfiniteLoopForIndex(value);
+        },
+    },
     methods: {
+        makeInfiniteLoopForIndex(value) {
+            const worksAmountFromZero = this.works.length - 1;
+            if(value > worksAmountFromZero) {
+                this.currentIndex = 0;
+            }
+            if(value < 0) {
+                this.currentIndex = worksAmountFromZero;
+            }
+        },
+        handleSlide(direction) {
+            switch(direction) {
+                case 'next' :
+                    this.currentIndex++;
+                    break;
+                case 'prev' :
+                    this.currentIndex--;
+                    break;
+            }
+        },
         makeArrWithRequiredImages(array) {
             return array.map((item) => {
                 const requirePic = require(`../images/content/${item.photo}`);
@@ -47,6 +83,5 @@ new Vue ({
     created() {
         const data = require("../data/works.json");
         this.works = this.makeArrWithRequiredImages(data);
-        this.currentWork = this.works[0];
     },
 });
